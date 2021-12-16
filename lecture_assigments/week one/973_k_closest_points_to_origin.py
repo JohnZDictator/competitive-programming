b5d001e63ce2cfa0741e8974fdb9ccdb7f96e5bb
YohannesKetema <yohannesketemazeleke@gmail.com>
2021-12-16 22:04:42 +0300

    973 k closest points to origin -- leetcode

--- lecture_assigments/week one/973_k_closest_points_to_origin.py
+++ lecture_assigments/week one/973_k_closest_points_to_origin.py
@@ -1,33 +1,20 @@
-
-# >>>>>>>> Time Limit exceeded and failed <<<<<<<<<< 
-# class Solution:
-#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-#         # point_distance = {}
-#         origin = [0, 0]
-#         distances = []
-#         result = []
-#         # calculate distance for each point relative from origin 
-#         for x in range(len(points)):
-#             point = points[x]
-#             distance = ((point[0]-origin[0])**2+(point[1]-origin[1])**2)
-#             # point_distance[distance] = point
-#             distances.append(distance)
-#         # print(point_distance[10])
-#         # for x in range(len(point_distance)):
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        sq_dists= []
+        result = []
         
-#         # Time limit exceeded
-#         # sort it out to make it easier in distance incrementally
-#         for x in range(len(distances)):
-#             min_distance = distances[x]
-#             min_point = points[x]
-#             for y in range(x+1, len(distances)):
-#                 if min_distance > distances[y]:
-#                     min_distance, distances[y] = distances[y], min_distance
-#                     min_point, points[y] = points[y], min_point    
-#             distances[x] = min_distance
-#             points[x] = min_point
+        for i in range(len(points)):
+            sq_dist = self.squared_distance(points[i])
+            sq_dists.append(sq_dist)
         
-#         # return k number of items
-#         for i in range(k):
-#             result.append(points[i])
-#         return result
+        for i in range(k):
+            min_dist_index = sq_dists.index(min(sq_dists))
+            result.append(points[min_dist_index])
+            sq_dists.pop(min_dist_index)
+            points.pop(min_dist_index)
+            
+            
+        return result
+       
+    def squared_distance(self, point: List[int]) -> int:
+        return point[0]**2 + point[1]**2
