b5b8dbf883d6b8f5988a9951a4ff201b32064ec9
JohnZDictator <yohannesketemazeleke@gmail.com>
2022-02-06 21:13:33 +0300

    Delete Node in a Linked List #237 --leetcode

--- a2sv/week three/Code for 1.py
+++ a2sv/week three/Code for 1.py
@@ -1,6 +1,4 @@
-n = int(input())
-start = int(input())
-end = int(input())
+n, start, end = map(int, input().split())
 
 def countOnes(bits, start, end):
     counter = 0
@@ -11,13 +9,45 @@ def countOnes(bits, start, end):
     return counter
 
 def toBit(n):
-    if n == 1:
+    if n <= 1:
         return 1
     
-    return str(toBit(n//2)) + str(n%2) + str(toBit(n//2))
-    
+    return str(n%2) + str(toBit(n//2))
+
+def reflector(bits):
+    return str(bits[1:]) + bits
+
 bits = toBit(n)
-print(bits)
-print(countOnes(bits, start, end))
+print(type(bits))
+refBit = reflector(bits)
+print(type(refBit))
+print(len(bits))
+print(len(refBit))
+print(countOnes(refBit, start, end))
+    
+    
+
+# n, start, end = map(int, input().split())
+
+# def countOnes(bits, start, end):
+#     counter = 0
+#     while start <= end:
+#         if bits[start-1] == '1':
+#             counter += 1
+#         if start < end and bits[end-1] == '1':
+#             counter += 1
+#         start += 1
+#         end -= 1
+#     return counter
+
+# def toBit(n):
+#     if n == 1:
+#         return 1
+    
+#     return str(toBit(n//2)) + str(n%2) + str(toBit(n//2))
+    
+# bits = toBit(n)
+# print(bits)
+# print(countOnes(bits, start, end))
     
     
