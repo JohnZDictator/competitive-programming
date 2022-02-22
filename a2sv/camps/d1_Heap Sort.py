c2f771f3efbe8ddb8aee880229a3a004d533061c
JohnZDictator <yohannesketemazeleke@gmail.com>
2022-02-23 02:50:30 +0300

    Heap Sort #day_1 #heap sort 1 --geeksforgeeks

--- a2sv/camps/d1_Heap Sort.py
+++ a2sv/camps/d1_Heap Sort.py
@@ -10,30 +10,40 @@ class Solution:
             self.heapify(arr, n, i)
     #Function to sort an array using Heap Sort.    
     def HeapSort(self, arr, n):
-        self.remove(arr, n)
-        arr.reverse()
-    def remove(self, arr, n):
         self.buildHeap(arr, n)
-        arr[0], arr[n-1] = arr[n-1], arr[0]
-        if n > 0:
-            self.remove(arr, n-1)
+        for i in range(n):
+            self.remove(arr, n-i)
+        arr.reverse()
+    
     def upheap(self, arr, n, i):
         parent = self.getParent(i)
         if i > 0 and arr[parent] > arr[i]:
             arr[parent], arr[i] = arr[i], arr[parent]
             i = parent
             self.upheap(arr, n , i)
+    
     def downheap(self, arr, n, i):
         leftChild = self.leftChild(i)
         rightChild = self.rightChild(i)
         minIndex = leftChild
-        if minIndex < n and rightChild < n and arr[rightChild] < arr[leftChild]:
+        if minIndex < n and rightChild < n  and arr[rightChild] < arr[leftChild]:
             minIndex = rightChild
         if minIndex < n and arr[minIndex] < arr[i]:
             arr[minIndex], arr[i] = arr[i], arr[minIndex]
             i = minIndex
             self.downheap(arr, n, i)
-        
+    
+    def remove(self, arr, n):
+        arr[0], arr[n-1] = arr[n-1], arr[0]
+        self.heapify(arr, n-1, 0)
+            
+    def insert(self, arr, element):
+        arr.append(element)
+        self.heapify(arr, n+1, n)
+    def update(self, arr, n, i, element):
+        arr[i] = element
+        self.heapify(arr, n, i)
+    
     def getMin(self, arr):
         return arr[0]
     
@@ -45,10 +55,3 @@ class Solution:
     
     def rightChild(self, i):
         return 2*i+2
-
-if __name__ == '__main__':
-    s = Solution()
-    # print(s.buildHeap([4,1,3,9,7], 5))
-    for i in range(10):
-        print(i)
-        print(s.downheap([10, 9,8,7,6,5,4,3,2,1], 10, i))
